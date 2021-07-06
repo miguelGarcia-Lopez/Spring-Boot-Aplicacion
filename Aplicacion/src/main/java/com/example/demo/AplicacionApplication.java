0ece11b260cc511c56ce2668d5aa8688ed111cf9
miguelGarcia-Lopez <63314248+miguelGarcia-Lopez@users.noreply.github.com>
2021-07-06 16:06:09 -0500

    Comit inicial con eclipse. Close #1

--- Aplicacion/src/main/java/com/example/demo/AplicacionApplication.java
+++ Aplicacion/src/main/java/com/example/demo/AplicacionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Aplicasion2Application {
+public class AplicacionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Aplicasion2Application.class, args);
+		SpringApplication.run(AplicacionApplication.class, args);
 	}
 
 }
